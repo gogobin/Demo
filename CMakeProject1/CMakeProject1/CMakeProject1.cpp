a7db8c945224950428540ff72862a353441271c0
liubin <2469619711@qq.com>
2021-09-19 09:35:46 +0800

    add new ProjectSettings

---
+++ CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,16 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+extern int SUM_UI;
+
+int main()
+{
+	int aa = 1;
+	int bb = 1;
+	cout << "Hello CMake." << endl;
+	cout << SUM_UI << endl;
+	return 0;
+}
---
+++ CMakeProject1/CMakeProject1/hello.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+extern int SUM_UI;
+extern int sum(int* a, int* b);
+int sum_number;
+
+#endif;
